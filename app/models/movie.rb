ab2681c5d24a2790dfc1e8bde0307a68a2d02efa
khaledmustafa91 <khaled.mustafa@cis.asu.edu.eg>
2024-02-13 01:00:40 +0200

    Add model validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,6 @@ class Movie < ApplicationRecord
     left_outer_joins(:reviews).select('AVG(reviews.stars) as star_avg, movie_id as id').group(:movie_id).order('star_avg')
   }
   scope :search, -> (query) { where("actor ILIKE :query", query: query)}
+
+  validate_presence_of :name, :description, :year, :actor, :director, :filming_location, :country
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
-
   belongs_to :movie
+  validate_presence_of :user, :stars, :body
 end
